7375c28569112249c45d36bea4c069f33629bea6
justinchiao <justinc.usatf@gmail.com>
2024-04-13 21:56:08 -0700

    test

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -20,5 +20,5 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1319/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1319/ --inputs "$file"
 done
